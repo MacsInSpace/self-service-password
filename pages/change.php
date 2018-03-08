1df793bab2ee0eaed63649abde03cf5dca403e16
Craig Hair <craig.hair@gmail.com>
2018-03-08 11:16:31 +1100

    Update change.php

--- pages/change.php
+++ pages/change.php
@@ -178,10 +178,10 @@ if ( $result === "true" ) {
 #==============================================================================
 if ( !empty($toggleextra)) {
 
-	if ( $result === "DET: Change Password Success" ) {
+	if ( $result === "" ) {
 			#echo "changing det pw";
 			$result = change_det_pw($hiddenarealogin, $hiddenareapassword, $newpassword, $confirmpassword);
-			if ( $result !== "DET: Change Password Success" ) {
+			if ( $result !== "" ) {
 				$result = "DET error: $result";
 			}	
 	}
